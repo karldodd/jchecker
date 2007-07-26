e5b2926a23d175c30aa34015bfa4e4c669a0ca34
li.jiang79 <li.jiang79@4d321573-1536-0410-80c3-eb697f851583>
2007-07-26 09:30:04 +0000

    prover dev cont'd

--- prover/ProverImplFociVampyre.java
+++ prover/ProverImplFociVampyre.java
@@ -1,22 +1,23 @@
 package prover;
 
 import tokens.*;
+import abstraction.*;
 
 import java.lang.*;
 import java.util.*;
 import java.io.*;
 
-public class ProverImplFociVampyre extends Prover{
-	private static ProverImplFociVampyre prover;
+public class ProverImplFociVampyre implements Prover{
+	private static ProverImplFociVampyre prover=null;
 	private ProverImplFociVampyre(){}
 
-	public static getInstance(){
+	public static Prover getInstance(){
 		if(prover==null)prover=new ProverImplFociVampyre();
-		return prover;
+		return (Prover)prover;
 	}
 
 	//if the edges are satisfiable, throw Exception
-	Set<Predicate> getInterpolation(Set<EdgeLabel> edges) throws Exception{
+	public Set<Predicate> getInterpolation(Set<EdgeLabel> edges) throws Exception{
 		StringBuilder sb=new StringBuilder("");
 		boolean firstElement=true;
 		for(EdgeLabel e : edges){
@@ -27,49 +28,88 @@ public class ProverImplFociVampyre extends Prover{
 			sb.append(e.toLFString());			
 		}
 		//then write the string to a temp file
+		File f=writeStringToTempFile(sb.toString());
+		return null;
 	}
 
 	//to tell if c1 implies c2
-	boolean imply(AdvCondition c1, AdvCondition c2){
-		return implyByVamPyre(c1.toLFString(),c2.toLFString());
+	public boolean imply(AdvCondition c1, AdvCondition c2) throws Exception{
+		return implyByVampyre(c1.toLFString(),c2.toLFString());
 	}
 
 	//to tell if the condition is satisfiable
-	boolean isSatisfiable(AdvCondition c);
+	public boolean isSatisfiable(AdvCondition c)
+	{
+		return false;
+		//throw new Exception("to be implemented...");
+	}
 
-	private	boolean implyByVampyre(String c1lf,String c2lf){
-		String source="pf: (=> "+c1lf+" "+c2lf+" )";
+	private	boolean implyByVampyre(String c1lf,String c2lf) throws Exception{
+		boolean result=false;
+		boolean gotresult=false;
+		String source="V1:\n pf: (=> "+c1lf+" "+c2lf+" )";
 		//then write the string to a temp file
 		File f=writeStringToTempFile(source);
 		
-		Process vampyreProcess;
+		Process vampyreProcess=null;
 		try {               
-	       		vampyreProcess = Runtime.getRuntime().exec(new String[]{"./prover.byte",f.getAbsolutPath()});
+	       		vampyreProcess = Runtime.getRuntime().exec(new String[]{"./prover.byte",f.getAbsolutePath()});
 		}     
      		catch(IOException e) {
 			System.err.println("Error when invoking vampyre:\n" + e); 
  			System.exit(1);
 		}
-		//InputStream[] inStreams = new InputStream[] {process.getInputStream(),process.getErrorStream()};
-		BufferedReader br  = new BufferedReader(new InputStreamReader(vampyreProcess.getInputStream()));
-		StreamTokenizer st=new StreamTokenizer(br);
-		st.commentChar("%");
-		st.eolIsSignificant(false);
 
-		vampyreProcess.waitFor();
+		try{
+			//InputStream[] inStreams = new InputStream[] {process.getInputStream(),process.getErrorStream()};
+			BufferedReader br  = new BufferedReader(new InputStreamReader(vampyreProcess.getInputStream()));
+			StreamTokenizer st=new StreamTokenizer(br);
+			st.commentChar('%');
+			st.eolIsSignificant(false);
+
+			vampyreProcess.waitFor();
 		
-		boolean streamEnd=false;
-		while(!streamEnd){
-			int tok=st.nextToken();
-			if(tok==st.TT_EOF)streamEnd=true;
-			else{
-				
+			boolean streamEnd=false;
+			while(!streamEnd){
+				int tok=st.nextToken();
+				if(tok==st.TT_EOF)streamEnd=true;
+				else{
+					if(tok==st.TT_WORD){
+						if(st.sval.equals("successfully")){
+							result=true;
+							streamEnd=true;
+							gotresult=true;
+						}
+						else if (st.sval.equals("Error")){
+							result=false;
+							streamEnd=true;
+							gotresult=true;
+						}
+					}
+				}
 			}
+			br.close();
+			
 		}
-				
+		catch(Exception e)
+		{
+			throw e;
+		}
+		if(!gotresult)
+			throw new Exception("result is null when invoking implyByVampyre");
+		return result;
 	}
 
-	private File writeStringToTempFile(String s){
-		
+	private File writeStringToTempFile(String s) throws Exception{
+		Calendar rightNow = Calendar.getInstance(); 
+		String fileName=Long.toString(rightNow.getTimeInMillis());
+
+
+		File tempFile=File.createTempFile("f","v");
+		//tempFile.deleteOnExit();
+		FileWriter fw=new FileWriter(tempFile);
+		fw.write(s);
+		fw.close();
+		return tempFile;
 	}
 }

208b604ae1c5cfeee813a824427a4408cd6fa540
li.jiang79 <li.jiang79@4d321573-1536-0410-80c3-eb697f851583>
2007-08-01 16:23:44 +0000

    platform pseudo-code without backTrace()

---
+++ abstraction/PredicateVector.java
@@ -0,0 +1,21 @@
+public class PredicateVector
+{
+	Predicate p;
+	State s;
+
+	PredicateVector(Predicate pred, State stat)
+	{
+		p = pred;
+		s = stat;
+	}
+
+	Predicate getPredicate()
+	{
+		return p;
+	}
+
+	State getState()
+	{
+		return s;
+	}
+}

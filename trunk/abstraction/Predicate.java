dbee8029aabec403c523fdcb71b8ec4b99a20941
karldodd <karldodd@4d321573-1536-0410-80c3-eb697f851583>
2007-08-02 08:02:07 +0000

    the Predicate class now has a getAdvConditionByState(State) method

--- trunk/abstraction/Predicate.java
+++ trunk/abstraction/Predicate.java
@@ -19,4 +19,20 @@ public class Predicate {
 	public String toLFString(){
 		return c.toLFString();
 	}
+	
+	public AdvCondition getAdvConditionByState(State s){
+		//public enum State {STATE_FALSE, STATE_POS, STATE_NEG, STATE_TRUE}
+		if(s==State.STATE_FALSE)return new AdvCondition(new Condition(false));
+		else if(s==State.STATE_POS)return this.c.clone();
+		else if(s==State.STATE_NEG){
+			AdvCondition temp=this.c.clone();
+			temp.negateSelf();
+			return temp;
+		}
+		else if(s==State.STATE_TRUE)return new AdvCondition(new Condition(true));
+		else{
+			System.err.println("Unexpected state of a predicate, returning null");
+			return null;
+		}
+	}
 }

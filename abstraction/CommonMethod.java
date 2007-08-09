33a4e3b1136f7cd9c38629b51f86e4b823c3507c
li.jiang79 <li.jiang79@4d321573-1536-0410-80c3-eb697f851583>
2007-08-09 13:38:47 +0000

    rewrite abstraction

--- abstraction/CommonMethod.java
+++ abstraction/CommonMethod.java
@@ -23,4 +23,49 @@ public class CommonMethod
 	{
 		System.out.println(s);
 	}
+	
+	public static void display(ArrayList<Edge> eTrace, ArrayList<Node> nTrace)
+	{
+		for (int i=0; i<eTrace.size(); i++)
+		{
+			System.out.print(nTrace.get(i).id + "  " + eTrace.get(i).id);
+			System.out.println("");
+		}
+		System.out.println(nTrace.get(nTrace.size()-1).id);
+	}
+
+	public static void display(ArrayList<Edge> eTrace)
+	{
+		System.out.print(eTrace.get(0).headNode.id);
+		for (Edge e : eTrace)
+		{
+			System.out.print(" " + e.tailNode.id);
+		}
+	}
+	
+	public static void display(ArrayList<Predicate> list)
+	{
+		for (Predicate l : list)
+		{
+			l.display();
+		}
+	}
+	
+	public static void display(ArrayList<StateSpace> list)
+	{
+		for (StateSpace l : list)
+		{
+			l.display();
+		}
+	}
+	
+	public static void print(String s)
+	{
+		System.out.println(s);
+	}
+	
+	public static println()
+	{
+		System.out.println();
+	} 
 }

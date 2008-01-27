cac49336cb9780abc1ead4e0ad95b100af374d46
li.jiang79 <li.jiang79@4d321573-1536-0410-80c3-eb697f851583>
2008-01-27 08:10:55 +0000

    add comments of State.java and Predicate.java

--- abstraction/State.java
+++ abstraction/State.java
@@ -1,5 +1,21 @@
 package abstraction;
 
-//The following states mean respectively:  predicate is empty, pedicate is postive, predicate is negtive, predicate is universal
-public enum State {STATE_FALSE, STATE_POS, STATE_NEG, STATE_TRUE}
+/**
+*枚举变量State的四个值分别对应谓词的四个状态：
+*<ul>
+*<li><code>STATE_FALSE</code> 表示谓词的bottom状态</li>
+*<li><code>STATE_POS</code> 表示谓词的true状态</li>
+*<li><code>STATE_NEG</code> 表示谓词的false状态</li>
+*<li><code>STATE_TRUE</code> 表示谓词的top状态</li>
+*</ul>
+*
+*@author Li Jiang
+*/
+
+public enum State {
+	STATE_FALSE, 
+	STATE_POS, 
+	STATE_NEG, 
+	STATE_TRUE
+}
 

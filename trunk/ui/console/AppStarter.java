b032ce22fb4fa69356ca4dc2e5394f835c4ab364
karldodd <karldodd@4d321573-1536-0410-80c3-eb697f851583>
2008-02-27 10:10:39 +0000

    test subclipse and remove some trash

--- trunk/ui/console/AppStarter.java
+++ trunk/ui/console/AppStarter.java
@@ -51,8 +51,7 @@ public class AppStarter{
 	    CFAGraph graph=CFAGraph.createCFAGraphFromCode(sourceFile);
 //	    print("CFAGraph constructed.");
 //	    graph.display();
-
-	    print("");
+	    
 	    CFATree tree = new CFATree(graph);
 	    ArrayList<Predicate> list = new ArrayList<Predicate>();
 	    list.add(new Predicate(new AdvCondition(new Condition(new Expression(new Variable("b")), new Expression(0), ConType.equal))));

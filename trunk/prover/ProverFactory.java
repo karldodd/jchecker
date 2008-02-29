01e4b7efc7131ae8c99e0c3787dfdb9d4e792c8d
karldodd <karldodd@4d321573-1536-0410-80c3-eb697f851583>
2008-02-29 05:16:58 +0000

    try junit and its integration with Ant

--- trunk/prover/ProverFactory.java
+++ trunk/prover/ProverFactory.java
@@ -6,10 +6,16 @@ import java.lang.*;
 import java.util.*;
 
 /**
-*没有用到的类，测试用
+*定理证明器的工厂类，Prover类的实例由这里获得
 */
 public class ProverFactory{
 
+	/**
+	 * 根据给定的名字构造并返回定理证明器的方法
+	 * @param name 定理证明器的名字
+	 * @return 定理证明器的实例
+	 * @throws Exception 未找到指定定理证明器的异常
+	 */
 	public static Prover getProverByName(String name) throws Exception{
 		if(name.equals("focivampyre")){
 			return ProverImplFociVampyre.getInstance();

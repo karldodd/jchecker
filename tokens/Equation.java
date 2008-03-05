492d1a4946ec1e7478751150a2484982092ce57d
xianwenchen1986 <xianwenchen1986@4d321573-1536-0410-80c3-eb697f851583>
2008-03-05 06:35:32 +0000

    实现单纯形的基本框架

---
+++ tokens/Equation.java
@@ -0,0 +1,26 @@
+package tokens;
+import java.lang.*;
+import java.util.*;
+
+/**
+ * 可传递的表达式，包含有其所属域的信息
+ *
+ * @author Chen Xianwen
+ */
+public class Equation{
+    /**
+     * 等式左端的表达式
+     */
+    public PassableExpression leftExpression;
+
+    /**
+     * 等式右端的常数
+     */
+    public double rightValue;
+
+    /**
+     * 该表达式的所属域
+     */
+    public int belongFieldId;
+}
+

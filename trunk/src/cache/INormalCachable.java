c617b706af56487d32f2bd887cf0f72b0c57b02f
zhoumin03 <zhoumin03@4d321573-1536-0410-80c3-eb697f851583>
2008-03-18 05:19:11 +0000

    MOD: interface of IPrefixCachable and INormalCachable

--- trunk/src/cache/INormalCachable.java
+++ trunk/src/cache/INormalCachable.java
@@ -1,5 +1,12 @@
 package cache;
 
+/**
+ * 一般的可以Cache的类型
+ * 最好是Immutable的，因为在Cacher里面存储了若干个INormalCachable的实例，并通过一定结构管理。
+ * 如果Key是能够修改的，则可能在管理的时候出现没有update的情况，而实时update的代价太高。
+ * @author aleck
+ *
+ */
 public interface INormalCachable extends ICachable {
 
 }
--- trunk/src/cache/IPrefixCachable.java
+++ trunk/src/cache/IPrefixCachable.java
@@ -1,9 +1,22 @@
 package cache;
 
+/**
+ * 同时实现了prefixHashCode的ICachable类
+ * 用于某些应用下，如果查找不到Key对应的Value，则转而查找Key的子集对应的Value。
+ * @author aleck
+ *
+ */
 public interface IPrefixCachable extends ICachable {
 	/**
 	 * 满足前缀关系的HashCode
 	 * @return
 	 */
 	public PrefixHashCode prefixHashCode();
+	
+	/**
+	 * 判断是否是某一个对象的prefix
+	 * @param that
+	 * @return
+	 */
+	public boolean isPrefixOf(IPrefixCachable that);
 }

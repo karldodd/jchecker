bc08cdd1699be99088ed26463e12f36e706d62be
karldodd <karldodd@4d321573-1536-0410-80c3-eb697f851583>
2008-03-22 09:45:37 +0000

    disjoint set implentation Step 2

--- trunk/src/prover/impl/xxx/egraph/utils/DisjointSetImpl/DisjointSet.java
+++ trunk/src/prover/impl/xxx/egraph/utils/DisjointSetImpl/DisjointSet.java
@@ -1,5 +1,8 @@
 package prover.impl.xxx.egraph.utils.DisjointSetImpl;
 
+import java.lang.annotation.Inherited;
+import java.util.Iterator;
+
 
 /**
  * 实现不相交集合的数据结构，采用不相交集合森林算法并在其基础上添加遍历集合的链表项
@@ -8,27 +11,35 @@ package prover.impl.xxx.egraph.utils.DisjointSetImpl;
  *
  */
 
-public class DisjointSet {
-	private Node headNode;
+public class DisjointSet<K> implements Iterable<Node<K>>{
+	private Node<K> headNode;
 	private int size;
-	private DisjointSet(Object atom){
+	protected DisjointSet(K atom){
 		this.size=1;
-		headNode=new Node(atom);
-		headNode.nextNode=null;
-		headNode.parent=null;
+		headNode=new HeadNode<K>(this,atom);
 	}
 	
-	public static DisjointSet makeSet(Object atom){
-		return new DisjointSet(atom);
+	public Node<K> getHeadNode(){
+		return headNode;
 	}
 	
-	public static DisjointSet findSet(Object atom){
-		///? how to find the node by atom?
-		/// Where to preserve a hash set from atom to node?
-		return null;
+	@Override public int hashCode(){
+		return headNode.hashCode();
+	}	
+	
+//	@Override public boolean equals(Object o){
+//		if(o instanceof DisjointSet){
+//			
+//			(DisjointSet<K>(o))
+//		}
+//		return false;
+//	}
+	
+	public int getSize(){
+		return size;
 	}
 	
-	public static DisjointSet union(DisjointSet ds1, DisjointSet ds2){
-		return null;
+	public Iterator<Node<K>> iterator(){
+		return new DisjointSetIterator<Node<K>>(this.headNode);
 	}
 }

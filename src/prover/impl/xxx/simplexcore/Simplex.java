95200de3f4e0d5b59d82c9c40b3eca8e7d9d93b3
xianwenchen1986 <xianwenchen1986@4d321573-1536-0410-80c3-eb697f851583>
2008-03-25 16:16:17 +0000

    An incompleted implementation of converting ConditionList to SimplexStruct. Together with the tests.

--- src/prover/impl/xxx/simplexcore/Simplex.java
+++ src/prover/impl/xxx/simplexcore/Simplex.java
@@ -27,26 +27,18 @@ public class Simplex{
      * 对当前的单纯形结构体进行一次出基入基的单纯形表运算
      *
      * @param simplexStruct 单纯形结构体
-     * @param wantMin 是否要求最小值
-     * @return 是否已终止
+     * @return 是否可以继续进行单纯形运算
      */
-    public boolean StepSimplex(SimplexStruct simplexStruct, boolean wantMin) {
+    public static boolean StepSimplex(SimplexStruct simplexStruct) {
+        return true;
+        /*
         int inBasis;
 
-        if(wantMin) {
-            // 进基符号取最大正数
-            inBasis = max(simplexStruct.tokens);
-            if(simplexStruct.tokens[inBasis].Value <= 0) {
-                // 单纯形表检验数不存在正数
-                return true;
-            }
-        } else {
-            // 进基符号取最小负数
-            inBasis = min(simplexStruct.tokens);
-            if(simplexStruct.tokens[inBasis].Value >= 0) {
-                // 单纯形表检验数不存在负数
-                return true;
-            }
+        // 进基符号取最大正数
+        inBasis = max(simplexStruct.tokens);
+        if(simplexStruct.tokens[inBasis].Value <= 0) {
+            // 单纯形表检验数不存在正数
+            return false;
         }
 
         // 出基符号取进基变量所属列最小的正数
@@ -59,14 +51,14 @@ public class Simplex{
 
         // 若进基变量所属列不含正数, 说明该式不可满足.
         if(outBasis < 0)
-            return true;
+            return false;
         
         // 进行出基入基运算
         // 本行归一化
         double rate = 1.0 / simplexStruct.tokenValues[outBasis][inBasis];
         for(int i = 0; i < simplexStruct.tokenValues[outBasis].length; ++i)
             simplexStruct.tokenValues[outBasis][i] *= rate;
-        simplexStruct.basis[outBasis].Value *= rate;
+        //simplexStruct.basis[outBasis].Value *= rate;
 
         // 行变换使其他行的相应列消为0
         for(int i = 0; i < simplexStruct.tokenValues.length; ++i) {
@@ -77,7 +69,7 @@ public class Simplex{
             for(int j = 0; j < simplexStruct.tokenValues[i].length; ++j) {
                 simplexStruct.tokenValues[i][j] -= simplexStruct.tokenValues[outBasis][j] * rate;
             }
-            simplexStruct.basis[i].Value -= simplexStruct.basis[outBasis].Value * rate;
+//            simplexStruct.basis[i].Value -= simplexStruct.basis[outBasis].Value * rate;
         }
 
         // 检验数的相应列也消为0
@@ -86,29 +78,14 @@ public class Simplex{
             for(int i = 0; i < simplexStruct.tokens.length; ++i) {
                 simplexStruct.tokens[i].Value -= simplexStruct.tokenValues[outBasis][i] * rate;
             }
-            simplexStruct.funcValue -= simplexStruct.basis[outBasis].Value * rate;
+//            simplexStruct.funcValue -= simplexStruct.basis[outBasis].Value * rate;
         }
 
         // 执行出基入基操作
-        simplexStruct.basis[outBasis].name = simplexStruct.tokens[inBasis].name;
+//        simplexStruct.basis[outBasis].name = simplexStruct.tokens[inBasis].name;
 
-        return false;
-    }
-
-    /**
-     * 求数组中的最小元素
-     *
-     * @param theArray 输入的数组
-     * @return 最小元素的序号
-     */
-    private int min(ValueVariable[] theArray) {
-        int result = 0;
-        for(int i = 1; i < theArray.length; ++i) {
-            if (theArray[i].Value < theArray[result].Value) {
-                result = i;
-            }
-        }
-        return result;
+        return true;
+        */
     }
 
     /**
@@ -116,8 +93,7 @@ public class Simplex{
      *
      * @param theArray 输入的数组
      * @return 最大元素的序号
-     */
-    private int max(ValueVariable[] theArray) {
+    private static int max(ValueVariable[] theArray) {
         int result = 0;
         for(int i = 1; i < theArray.length; ++i) {
             if (theArray[i].Value > theArray[result].Value) {
@@ -126,5 +102,6 @@ public class Simplex{
         }
         return result;
     }
+     */
 
 }

6bda3700db31cc3684eb17162377ad1dc1522a71
xianwenchen1986 <xianwenchen1986@4d321573-1536-0410-80c3-eb697f851583>
2008-03-28 04:10:55 +0000

    Make the construct of SimplexStruct more powerful. And added some corresponding tests.

--- test/prover/impl/xxx/determination/Theory_LI/SimplexTester.java
+++ test/prover/impl/xxx/determination/Theory_LI/SimplexTester.java
@@ -91,5 +91,78 @@ public class SimplexTester {
         //   2  -1       1      0       0   9
         assertTrue(Simplex.Satisfiable(simplexStruct));
 	}
-}
 
+    /*
+     * Construct a simplex using the following statements:
+     *  a - 2b >= 3
+     *  a + b <= 2
+     *
+     * They are not satisfiable.
+     */
+	@Test
+	public void testSimplex3() throws Exception{
+        // a - 2b >= 3
+        Expression e1 = new Expression(new Variable("a"));
+
+        Expression e21 = new Expression(2);
+        Expression e22 = new Expression(new Variable("b"));
+        Expression e2 = new Expression(e21, e22, ExpType.multiply);
+
+        Expression e_left_1 = new Expression(e1, e2, ExpType.minus);
+        Expression e_right_1 = new Expression(3);
+        Condition condition1 = new Condition(e_left_1, e_right_1, ConType.equallarger);
+
+        // a + b <= 2
+        Expression e3 = new Expression(new Variable("a"));
+        Expression e4 = new Expression(new Variable("b"));
+
+        Expression e_left_2 = new Expression(e3, e4, ExpType.plus);
+        Expression e_right_2 = new Expression(2);
+        Condition condition2 = new Condition(e_left_2, e_right_2, ConType.equalsmaller);
+
+        conditions = new Vector<Condition>();
+        conditions.add(condition1);
+        conditions.add(condition2);
+
+        simplexStruct = new SimplexStruct(this.conditions);
+
+        assertTrue(!Simplex.Satisfiable(simplexStruct));
+	}
+
+    /*
+     * Construct a simplex using the following statements:
+     *  a - 2b >= 3
+     *  a + b <= 3
+     *
+     * They are satisfiable.
+     */
+	@Test
+	public void testSimplex4() throws Exception{
+        // a - 2b >= 3
+        Expression e1 = new Expression(new Variable("a"));
+
+        Expression e21 = new Expression(2);
+        Expression e22 = new Expression(new Variable("b"));
+        Expression e2 = new Expression(e21, e22, ExpType.multiply);
+
+        Expression e_left_1 = new Expression(e1, e2, ExpType.minus);
+        Expression e_right_1 = new Expression(3);
+        Condition condition1 = new Condition(e_left_1, e_right_1, ConType.equallarger);
+
+        // a + b <= 3
+        Expression e3 = new Expression(new Variable("a"));
+        Expression e4 = new Expression(new Variable("b"));
+
+        Expression e_left_2 = new Expression(e3, e4, ExpType.plus);
+        Expression e_right_2 = new Expression(3);
+        Condition condition2 = new Condition(e_left_2, e_right_2, ConType.equalsmaller);
+
+        conditions = new Vector<Condition>();
+        conditions.add(condition1);
+        conditions.add(condition2);
+
+        simplexStruct = new SimplexStruct(this.conditions);
+
+        assertTrue(Simplex.Satisfiable(simplexStruct));
+	}
+}
